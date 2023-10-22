f6411630865737ad4216161346e87a2dff7cf234
baibhavmandal <baibhavmandal1@gmail.com>
2023-10-22 16:45:59 +0530

    second update of game routes

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -2,23 +2,10 @@ import { Server } from "socket.io";
 
 let _ioGameParty;
 
-const origin = "https://moneyballstore.webpubsub.azure.com";
+// const origin = "https://moneyballstore.webpubsub.azure.com";
 
 const setIoGame = (server) => {
-  _ioGameParty = new Server(server, {
-    cors: {
-      methods: ["GET", "POST"],
-      allowedHeaders: [
-        "Origin",
-        "X-Requested-With",
-        "Content-Type",
-        "Accept",
-        "Authorization",
-      ],
-      credentials: true,
-      optionSuccessStatus: 200,
-    },
-  });
+  _ioGameParty = new Server(server);
 
   return _ioGameParty;
 };
